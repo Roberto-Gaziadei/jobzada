ae0ecf436697a5ea7d9016f4431b40323425221a
Roberto_Graziadei <roberto.2022315930@aluno.iffar.edu.br>
2024-09-11 12:16:47 -0300

    Praticamente pronto

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
       <a href="#" class="brand-logo"><img src="img/logo.png" height="55" width="60"></a>
     
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li <?php if($paginaCorrente == 'index02.php') {echo 'class="active"';}?>> <a class="black-text" href="#">Home</a></li>    
+        <li <?php if($paginaCorrente == 'index.php') {echo 'class="active"';}?>> <a class="black-text" href="#">Home</a></li>    
         <li <?php if($paginaCorrente == 'clientes.php') {echo 'class="active"'; } ?>> <a class="black-text" href="#">Clientes</a></li> 
         <li <?php if($paginaCorrente == 'quem.php') {echo 'class="active"'; } ?>><a class="black-text" href="#">Nós!</a></li>
       </ul>
---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body class="brown  lighten-5">
+  <?php include_once "header.php" ?>
+
+  <main class="container">
+
+    <div style="margin-top: 10px;">
+      <div class="row">
+        <div class="col s12 m12  center-align"> <img class="responsive-img circle small" src="img/thiago.png" width="120"> <br> <b>Thiago Cássio Krug</b> </div>
+        <div class="col s12 m12  center-align"><br> Professor do IFfar e programador </div>
+      </div>
+
+      <div class="row">
+        <div class="col s12 m12  center-align">
+
+          <a class="btn-floating btn-small waves-effect waves-light black"><i class="material-icons">email</i></a>
+          <a class="btn-floating btn-small waves-effect waves-light black"><i class="material-icons">add_shopping_cart</i></a>
+          <a class="btn-floating btn-small waves-effect waves-light black"><i class="material-icons">card_giftcard</i></a></a>
+
+        </div>
+
+      </div>
+
+      <div class="container">
+        <hr>
+
+        Thiago Cássio Krug é um profissional que atua como engenheiro de software, com experiência em desenvolvimento de sistemas e tecnologias avançadas. Ao longo de sua carreira, tem se destacado na criação de soluções inovadoras no campo da engenharia de software, sempre focado em otimizar processos e implementar metodologias eficientes. Com sólida formação acadêmica e uma abordagem prática, Thiago tem contribuído de maneira significativa para projetos que envolvem automação e controle de qualidade de sistemas, tornando-se uma referência no setor.<br>
+
+        <br><br>
+      </div>
+
+
+      <div class="col s12  m3 center-align"> </div>
+    </div>
+    </div>
+
+    <div class="row">
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/foto1.jpg">
+            <span class="card-title">Natureza</span>
+          </div>
+          <div class="card-content">
+            <p>Paisagem.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Roberto and Jeverson</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/foto2.jpg">
+            <span class="card-title">Natureza</span>
+          </div>
+          <div class="card-content">
+            <p>Paisagem.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Roberto and Jeverson</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/foto3.jpg">
+            <span class="card-title">Natureza</span>
+          </div>
+          <div class="card-content">
+            <p>Paisagem.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Roberto and Jeverson</a>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+    <div class="row">
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/foto1.jpg">
+            <span class="card-title">Natureza</span>
+          </div>
+          <div class="card-content">
+            <p>Paisagem.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Roberto and Jeverson</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/foto2.jpg">
+            <span class="card-title">Natureza</span>
+          </div>
+          <div class="card-content">
+            <p>Paisagem.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Roberto and Jeverson</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/foto3.jpg">
+            <span class="card-title">Natureza</span>
+          </div>
+          <div class="card-content">
+            <p>Paisagem.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Roberto and Jeverson</a>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+
+
+    <footer class="page-footer black">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Rodapé</h5>
+                <p class="grey-text text-lighten-4">Site de Roberto e Jeverson.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!"><img src="img/insta.png" width="30" height="30">Instagram</a> </li>
+                  <li><a class="grey-text text-lighten-3" href="#!"><img src="img/face.png" width="30" height="30"> Facebook </a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!"><img src="img/whats.png" width="28" height="28">Whatsapp  </a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2024 Alunos IFfar
+            </div>
+          </div>
+        </footer>
+
+
+
+
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+  </main>
+</body>
+
+</html>
